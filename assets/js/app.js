94b6395a30b1913f25a032031950770cf3954a4c
sakib6619 <sahadothosain6619@gmail.com>
2024-04-06 23:33:25 +0600

    aded contact and fotter

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,8 +23,8 @@ var typed = new Typed("#typed", {
   stringsElement: "#typed-strings",
   typeSpeed: 100,
   backSpeed: 100,
-  backDelay: 500,
-  startDelay: 1000,
+  backDelay: 200,
+  startDelay: 500,
   loop: true,
 });
 
